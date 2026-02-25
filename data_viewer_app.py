7ed2f47ea9ce95f500d8f1011366b8d5376d684e
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2026-02-25 13:06:28 +0000

    Update data_viewer_app.py

--- data_viewer_app.py
+++ data_viewer_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Create app
-st.image('./catsci-logo (1).svg')
+st.image('./catsci-logo (1).svg', width=300)
 st.title("ML Training Data Viewer")
 
 # Load data
@@ -12,26 +12,34 @@ if not datafile:
 data = pd.read_csv(datafile, sep=",")
 
 # Basic info
-st.header(datafile.name)
+st.header(datafile.name.replace(".csv",""))
 st.write(f"Number of rows: {data.shape[0]}")
 st.write(f"Number of columns: {data.shape[1]}")
+st.subheader("Dataframe Overview")
 st.table(data.describe())
-st.dataframe(data)
 
-# Single column information
+if st.toggle("View entire dataframe"):
+    st.dataframe(data)
+
+# Select a column
+# show fist X values, get data type, no. of missing values, median and mean if approrpriate, most common values
 st.divider()
+st.subheader("Single Column Information")
 data_column = st.selectbox(
     label = "Select a column to analyse",
     options = data.columns
 )
 
-if data_column:
-    st.write(data[data_column].head(10))
-    st.write(data[data_column].describe())
-    st.write(f"Most commmon values for {data_column}:")
+st.write(data[data_column].describe())
+
+if st.toggle(f"View value counts for {data_column}"):
     st.write(data[data_column].value_counts())
-    #st.pyplot(data[data_column].hist())
-    #st.pylot(data[[data_column]].boxplot())
+
+#st.pyplot(data[data_column].hist())
+#st.pyplot(data[[data_column]].boxplot())
+
+# Single column plots
+# histogram
+# box plot(s)
 
 # Pairwise plots
-# Multiple box plots
