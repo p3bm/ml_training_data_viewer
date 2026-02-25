783fe6f5aa602d18fc874ed2366792493feeccff
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2026-02-25 14:28:35 +0000

    Update data_viewer_app.py

--- data_viewer_app.py
+++ data_viewer_app.py
@@ -33,7 +33,7 @@ data_column = st.selectbox(
 
 st.write(f"Total number of values: {len(data[data_column])}")
 st.write(f"Data type: {data[data_column].dtypes}")
-st.write(f"Number of missing values: {data[data_column].isna()}")
+st.write(f"Number of missing values: {data[data_column].isna().sum()}")
 
 st.write(f"Overview of data in {data_column}:")
 st.write(data[data_column].describe())
