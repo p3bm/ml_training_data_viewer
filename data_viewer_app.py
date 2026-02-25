2de6524b046e91de2a0201e3239ca00d149d6b1e
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2026-02-25 14:12:18 +0000

    Update data_viewer_app.py

--- data_viewer_app.py
+++ data_viewer_app.py
@@ -38,7 +38,7 @@ st.write(f"Overview of data in {data_column}:")
 st.write(data[data_column].describe())
 
 st.write(f"Missing value information for {data_column}:")
-st.write(data[data_column].info())
+st.write(data[[data_column]].info())
 
 if st.toggle(f"View value counts for {data_column}"):
     st.write(data[data_column].value_counts())
