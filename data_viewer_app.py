7d4d38d882d95c26ad8a184cb1e2130886e7d0d9
Patrick Mann <148568948+p3bm@users.noreply.github.com>
2026-02-25 14:10:39 +0000

    Update data_viewer_app.py

--- data_viewer_app.py
+++ data_viewer_app.py
@@ -31,8 +31,8 @@ data_column = st.selectbox(
     options = data.columns
 )
 
-st.write(f"Total number of values: {len(data[data_column]}")
-st.write(f"Data type: {(data[data_column].dtypes}")
+st.write(f"Total number of values: {len(data[data_column])}")
+st.write(f"Data type: {data[data_column].dtypes}")
 
 st.write(f"Overview of data in {data_column}:")
 st.write(data[data_column].describe())
